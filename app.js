fdce5c9febfc28271e93abab86c25c37eda667fb
MaicolCode <paucarjhon07@outlook.com>
2024-06-09 20:09:46 -0500

    Creacion del POST e instalacion de zod para validar datos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const crypto = require('node:crypto')
+// const z = require('zod')
 const vehicles = require('./data/vehicles.json')
 const PORT = process.env.PORT ?? 6675
 
 app.disable('x-powered-by')
+app.use(express.json())
 app.get('/vehicles', (req, resp) => {
   const { year } = req.query
   const existYear = vehicles.findIndex(vehicle => vehicle.year === year)
@@ -24,6 +27,24 @@ app.get('/vehicles/:id', (req, resp) => {
   const vehicle = vehicles.find(vehicle => vehicle.id === id)
   if (vehicle) return resp.json(vehicle)
   return resp.status(400).json({ message: "Vehicle id don't exist." })
+})
+
+app.post('/vehicles', (req, resp) => {
+  const { name, model, year, description, price, urlImage } = req.body
+
+  const newVehicle = {
+    id: crypto.randomUUID(),
+    name,
+    model,
+    year,
+    description,
+    price,
+    urlImage
+  }
+
+  vehicles.push(newVehicle)
+
+  return resp.status(201).json(newVehicle)
 })
 
 app.listen(PORT, () => {
