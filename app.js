f8c820f0c8b998563b1de960db3d823df94f311e
MaicolCode <paucarjhon07@outlook.com>
2025-05-04 14:03:33 -0500

    Fix static file serving by correctly joining the directory path for public assets

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express, { json } from 'express'
 import { corsMiddleware } from './middlewares/cors.js'
 import { vehicleRouter } from './routes/vehicle.js'
+import { __dirname } from './utils/dirname.js'
+import { join } from 'path'
 
 export const createApp = ({ modelVehicle }) => {
   const PORT = process.env.PORT ?? 6675
   const app = express()
-
+  app.use(express.static(join(__dirname, '../web/public')))
   app.disable('x-powered-by')
   app.use(json())
   app.use(corsMiddleware)
